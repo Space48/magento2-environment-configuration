64d37bf68d8e5ffe9387ef28b758fc3296ab25fa
dank00 <dan.kenny@space48.com>
2018-01-04 15:45:48 +0000

    Add a console command to save config values for given environment

---
+++ src/EnvironmentConfigurationCommand.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Space48\EnvironmentConfiguration;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class EnvironmentConfigurationCommand extends Command
+{
+    /** @var ConfigValueRepository */
+    private $configValueRepository;
+
+    /** @var EnvironmentConfigValues */
+    private $environmentConfigValues;
+
+    public function __construct(
+        ConfigValueRepository $configValueRepository,
+        EnvironmentConfigValues $environmentConfigValues,
+        string $name = null
+    ) {
+        $this->configValueRepository = $configValueRepository;
+        $this->environmentConfigValues = $environmentConfigValues;
+
+        parent::__construct($name);
+    }
+
+    protected function configure()
+    {
+        parent::configure();
+
+        $this->setName('app:environment-configuration:apply');
+        $this->setDescription('Apply non-sensitive environment specific config values');
+        $this->addArgument(
+            'environment',
+            InputArgument::REQUIRED,
+            'The environment to configure'
+        );
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $environment = $input->getArgument('environment');
+
+        if (!Environment::isValid($environment)) {
+            throw new \InvalidArgumentException(sprintf(
+                'specified environment %s is invalid, accepted environments are %s.',
+                $environment,
+                implode(', ', Environment::all())
+            ));
+        }
+
+        $environmentValues = $this->environmentConfigValues->getConfigValuesByEnvironment($environment);
+
+        /** @var ConfigValue $configValue */
+        foreach ($environmentValues as $configValue) {
+            $this->configValueRepository->save($configValue);
+        }
+
+        $output->writeln(sprintf(
+            'Updated config values for environment %s',
+            $environment)
+        );
+
+        return 0;
+    }
+
+
+}
